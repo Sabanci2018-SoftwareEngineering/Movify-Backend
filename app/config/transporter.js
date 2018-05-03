5fa320331f2859ea1cd8acab262a173f0169806e
Oran Can Ören <orancanoren@gmail.com>
2018-05-03 18:38:31 +0300

    updated nodemailer initialization condition for consistency

--- app/config/transporter.js
+++ app/config/transporter.js
@@ -1,6 +1,6 @@
 var nodemailer = require('nodemailer');
 
-if (process.env.NODE_ENV != 'TEST') {
+if (process.env.NODE_ENV != 'PROD') {
   var transporter = nodemailer.createTransport({
     service: process.env.MAIL_SERVICE,
     auth: {
